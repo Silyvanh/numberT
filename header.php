774f537a231ef6fae51c858d7161e326055db27d
Silyvanh <silyvanhkongsadene.ecolesrp@gmail.com>
2017-06-05 17:07:42 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="langage" content="fr">
+    <meta name="author" content="Silyvanh">
+    <meta name="description" content="création du site web Number T">
+    <meta name="viewport" content="width=500, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Number T</title>
+  </head>
+  <body>
+    <header>
+
+    </header>
